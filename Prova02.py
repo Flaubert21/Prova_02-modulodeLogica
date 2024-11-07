51c0c1a08f359802bab26ab1d4fc013c0841cab4
Flaubert21 <97761286+Flaubert21@users.noreply.github.com>
2024-11-06 21:39:50 -0300

    Add files via upload

---
+++ Prova02.py
@@ -0,0 +1,10 @@
+n = int(input("Digite um número:"))
+
+if n > 0:
+    print("O número é positivo.")
+
+elif n < 0:
+    print("O número é negativo.")    
+
+elif n == 0:
+    print("O número é zero.")        
